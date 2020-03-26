f84d3732247fb491b8faca09cb1f99c387924f7d
Fube <fubeitch@gmail.com>
2020-03-25 20:00:09 -0400

    Debug

--- commands/banish.js
+++ commands/banish.js
@@ -50,9 +50,10 @@ const shuffle = new Command({
 
         let channels = []; 
 
-        for(field in message.guild.channels.cache){
-            console.log(field);
-        }
+        
+        console.log(message.guild.channels.cache)
+        console.log(message.guild.channels.client)
+
         message.guild.channels.filter(channel => channel.type == 'voice' && channel.permissionsFor(target).has(['CONNECT']) && channel.permissionsFor(client.user).has(['CONNECT','MOVE_MEMBERS'])).forEach(n => channels.push(n));
 
         let currChannel = target.voiceChannel;
