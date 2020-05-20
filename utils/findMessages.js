e663282edcca05a925d93898dfefb67386420bfd
Fube <fubeitch@gmail.com>
2020-05-20 16:16:35 -0400

    Debug

--- utils/findMessages.js
+++ utils/findMessages.js
@@ -3,10 +3,11 @@
  * @param {*} channel Where to look for the messages
  * @param {function} predicate Criteria to match
  */
-const findMessages = (channel, predicate) =>{
+const findMessages = async (channel, predicate) =>{
 
     const messages = [];
-    console.log(channel.messages.cache.size)
+    const foo = await channel.messages.fetch({},true);
+    console.log(channel.messages.cache)
     for(const msg of channel.messages.cache){
 
         console.log(msg)
