414dcc543fb92f7b570986e1c3f8bc97522486ad
Fube <fubeitch@gmail.com>
2020-05-20 18:40:26 -0400

    Debug

--- commands/chrono.js
+++ commands/chrono.js
@@ -56,7 +56,7 @@ const dealReset = new CronJob({
         const fun = async () => {
 
             const chronoChannels = findChannels('all', n => n.type =='text' && n.name == 'chrono');
-            console.log('Fun started')
+            console.log('Fun started', chronoChannels.length)
 
             for(const ch of chronoChannels){
 
