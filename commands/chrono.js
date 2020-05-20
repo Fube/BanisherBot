bbd7f0da682b8be59f5b467c642e5b226844a4f5
Fube <fubeitch@gmail.com>
2020-05-20 15:19:04 -0400

    Debug

--- commands/chrono.js
+++ commands/chrono.js
@@ -18,6 +18,7 @@ async function getNewDeal(){
 let chronoDeal;
 let chronoChannels = [];
 client.once('ready', () => {chronoChannels = findChannels('all', n => n.type =='text' && n.name == 'chrono');});
+console.log(client.readyAt)
 
 const makeEmbed = n => new Object({
 
@@ -51,12 +52,12 @@ const dealReset = new CronJob({
     cronTime : '00 03 12 * * *',
     onTick : async _ => {
         chronoDeal = await getNewDeal();
-        console.log(chronoChannels)
+
         for(const ch of chronoChannels){
 
             const foo = await ch.awaitMessages(n => n.author.id == client.user.id);
-            const bar = await ch.awaitMessages(n => n.author.id == client.user.id && Date.now() - n.createdAt >= 1000 * 60 ** 2 *24);
-            if(!foo.size || bar.size)
+            const bar = await ch.awaitMessages(n => n.author.id == client.user.id && Date.now() - n.createdAt >= 1000 * 60 ** 2 * 24);
+            if(!foo.size || [...bar].pop())
                 ch.send({embed : makeEmbed(chronoDeal)});
         }
     },
